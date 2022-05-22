f0d9cc6f360e6478c7c244e7a894a791c6a40569
Rohit <rohitkamble10792@gmail.com>
2022-05-22 16:22:49 +0530

    Welcome to Quotient and Remainder Program

---
+++ src/com/bridgelabz/qandr/QandR.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.qandr;
+
+public class QandR {
+    public static void main(String[] args) {
+        System.out.println("welcome to Quotient and Remainder Program");
+    }
+}
